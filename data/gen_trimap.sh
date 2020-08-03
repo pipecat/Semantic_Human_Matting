d801d3502c2f7cd6157f123c474bf17007da8aec
pipecat <pipecat@yahoo.com>
2020-08-03 23:43:27 +0800

    test generating mask

--- data/gen_trimap.sh
+++ data/gen_trimap.sh
@@ -1,7 +1,4 @@
 
 ROOT=./
 
-python3 gen_trimap.py \
-	--mskDir=$ROOT/mask \
-	--saveDir=$ROOT/trimap \
-	--list=./train.txt --size=10	
+python3 gen_trimap.py --mskDir=$ROOT/mask --saveDir=$ROOT/trimap --list=./train.txt --size=10	
