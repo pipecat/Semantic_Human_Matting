5774048b6d31cc41fad4e6453ba97225ed27cd9b
pipecat <pipecat@yahoo.com>
2020-08-04 00:04:15 +0800

    get a trimap

--- data/gen_trimap.py
+++ data/gen_trimap.py
@@ -22,7 +22,7 @@ def erode_dilate(msk, struc="ELLIPSE", size=(10, 10)):
 
     #msk = msk.astype(np.float32)
     msk = msk / 255
-    #msk = msk.astype(np.uint8)
+    msk = msk.astype(np.uint8)
 
     # val in 0 or 255
 
@@ -32,7 +32,7 @@ def erode_dilate(msk, struc="ELLIPSE", size=(10, 10)):
     cnt1 = len(np.where(msk >= 0)[0])
     cnt2 = len(np.where(msk == 0)[0])
     cnt3 = len(np.where(msk == 1)[0])
-    #print("all:{} bg:{} fg:{}".format(cnt1, cnt2, cnt3))
+    print("all:{} bg:{} fg:{}".format(cnt1, cnt2, cnt3))
     assert(cnt1 == cnt2 + cnt3)
 
     cnt1 = len(np.where(dilated >= 0)[0])
@@ -63,7 +63,7 @@ def main():
         print(msk_name)
         trimap_name = args.saveDir + "/" + name.strip()[:-4] + ".png"
         msk = cv2.imread(msk_name, 0)
-        trimap = erode_dilate(msk, size=(args.size,args.size))
+        trimap = erode_dilate(msk, "ELLIPSE", size=(args.size,args.size))
 
         print("Write to {}".format(trimap_name))
         cv2.imwrite(trimap_name, trimap)
